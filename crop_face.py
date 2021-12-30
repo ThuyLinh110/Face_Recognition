440354b22d126ef706143cdd40ee25e28676ab60
ThuyLinh110 <thuylinh1102001@gmail.com>
2021-12-30 12:35:24 +0700

    train model

--- crop_face.py
+++ crop_face.py
@@ -1,36 +1,16 @@
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import cv2 # opencv
-import mtcnn
 import os
-from mtcnn.mtcnn import MTCNN
+from keras_facenet import FaceNet
 from matplotlib import pyplot as plt
-from keras.models import load_model
-from PIL import Image
 
+
+# crop face from image and get embedding vector
 def extract_face(filename, required_size=(160, 160)):
-    # load image from file
-    image = Image.open(filename)
-    # convert to RGB, if needed
-    image = image.convert('RGB')
-    # convert to array
-    pixels = np.asarray(image)
-    # create the detector, using default weights
-    detector = MTCNN()
-    # detect faces in the image
-    results = detector.detect_faces(pixels)
-    # extract the bounding box from the first face
-    x1, y1, width, height = results[0]['box']
-    # deal with negative pixel index
-    x1, y1 = abs(x1), abs(y1)
-    x2, y2 = x1 + width, y1 + height
-    # extract the face
-    face = pixels[y1:y2, x1:x2]
-    # resize pixels to the model size
-    image = Image.fromarray(face)
-    image = image.resize(required_size)
-    face_array = np.asarray(image)
-    return face_array
+    embedd = FaceNet()
+    detections = embedd.extract(filename, threshold=0.95)
+    return detections[0]['embedding']
 
 
 def load_face(dir):
--- ex.py
+++ ex.py
@@ -1,11 +1,42 @@
-from crop_face import load_dataset
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import Normalizer
+from sklearn.svm import SVC
+from keras_facenet import FaceNet
 import numpy as np # linear algebra
-from keras.models import load_model
 
+from crop_face import extract_face
 
-
+# normalize input vectors
 data = np.load('image.npz')
-trainX, trainY = data['arr_0'], data['arr_1']
-# load the facenet model
-facenet_model = load_model('./facenet-keras/facenet_keras.h5')
-print('Loaded Model')
+emdTrainX, trainY = data['arr_0'], data['arr_1']
+in_encoder = Normalizer()
+emdTrainX_norm = in_encoder.transform(emdTrainX)
+print(emdTrainX_norm)
+
+# label encode targets
+out_encoder = LabelEncoder()
+out_encoder.fit(trainY)
+trainy_enc = out_encoder.transform(trainY)
+print(trainy_enc)
+
+# fit model
+model = SVC(kernel='linear', probability=True)
+model.fit(emdTrainX_norm, trainy_enc)
+
+# predict
+yhat_train = model.predict(emdTrainX_norm)
+print(yhat_train)
+
+
+# test
+testX ='./image_test/sontung.jpg'
+embedd = FaceNet()
+detections = embedd.extract(testX, threshold=0.95)
+
+emdTestX = detections[0]['embedding']
+# emdTestX_norm = in_encoder.transform(emdTestX)
+
+samples = np.expand_dims(emdTestX, axis=0)
+yhat_class = model.predict(samples)
+print(yhat_class)
