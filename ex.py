8dc565a6b26d2b9392e3454504c5e6b8f8522850
ThuyLinh110 <thuylinh1102001@gmail.com>
2021-12-30 14:03:12 +0700

    train model and test image

--- ex.py
+++ ex.py
@@ -3,7 +3,9 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import Normalizer
 from sklearn.svm import SVC
 from keras_facenet import FaceNet
+from matplotlib import pyplot as plt
 import numpy as np # linear algebra
+from PIL import Image
 
 from crop_face import extract_face
 
@@ -33,10 +35,21 @@ print(yhat_train)
 testX ='./image_test/sontung.jpg'
 embedd = FaceNet()
 detections = embedd.extract(testX, threshold=0.95)
-
 emdTestX = detections[0]['embedding']
-# emdTestX_norm = in_encoder.transform(emdTestX)
+
+x1, y1, width, height = detections[0]['box']
+x1, y1 = abs(x1), abs(y1)
+x2, y2 = x1 + width, y1 + height
+image = Image.open(testX)
+# convert to array
+pixels = np.asarray(image)
+plt.imshow(pixels[y1:y2, x1:x2])
+
 
 samples = np.expand_dims(emdTestX, axis=0)
+
 yhat_class = model.predict(samples)
 print(yhat_class)
+yhat_prob = model.predict_proba(samples)
+print(yhat_prob)
+plt.show()
